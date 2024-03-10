5a6dec626bd8f205aef914896d0257fe33315fce
stephen ogwu <116175711+stephenfoxx@users.noreply.github.com>
2024-03-10 18:33:40 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {},
+      fontFamily: {
+        mansalva: ['Mansalva'],
+      },
+    },
+  },
+  plugins: [],
+};
+
